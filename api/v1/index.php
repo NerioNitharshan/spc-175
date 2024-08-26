df0ccd508d31df7e3d00736b676beb344cba00cd
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2024-08-26 11:57:07 +0200

    feat: add timeline demo page

--- api/v1/index.php
+++ api/v1/index.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 0);
 ini_set("log_errors", "On");
 
-define("DEBUG", true);
+define("DEBUG", false);
 
 function error_handler(int $errno, string $errstr, string $errfile, int $errll)
 {
