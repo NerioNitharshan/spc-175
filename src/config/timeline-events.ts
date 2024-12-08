c8cfdaac9dc80d968b60c84fd2db6ddce7c27e8d
NerioNitharshan <57047561+NerioNitharshan@users.noreply.github.com>
2024-12-08 12:22:22 +0530

    Fixes

--- src/config/timeline-events.ts
+++ src/config/timeline-events.ts
@@ -6,30 +6,105 @@ interface Event {
 
 const TIMELINE_EVENTS: Array<Event> = [
     {
-        date: "1850",
-        title: "Inaguration of the College",
-        cover: "https://cdn.jsdelivr.net/gh/BirnadinErick/spc-cdn@master/about-st-pst-placeholder.webp"
+        date: "2025-03-14",
+        title: "Inauguration with Patricians' Walk & Magazine Release",
+        cover: "https://images.unsplash.com/photo-1544005313-94ddf0286df2" // Inauguration
     },
     {
-        date: "2000",
-        title: "2000 year",
-        cover: "https://cdn.jsdelivr.net/gh/BirnadinErick/spc-cdn@master/forest.webp"
-    }, {
-        date: "2009",
-        title: "something happened",
-        cover: "https://cdn.jsdelivr.net/gh/BirnadinErick/spc-cdn@master/hands.webp"
-    }, {
-        date: "2019",
-        title: "another thing happened",
-        cover: "https://unsplash.com/photos/NV5_g30gPfE/download?ixid=M3wxMjA3fDB8MXx0b3BpY3x8Q0R3dXdYSkFiRXd8fHx8fDJ8fDE3MjQ2NTk5NzV8&force=true"
-    }, {
-        date: "2020",
-        title: "Covid-19 happened",
-        cover: "https://unsplash.com/photos/w9KEokhajKw/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Mnx8Y29yb25hfGVufDB8fHx8MTcyNDY2NDI4MXww&force=true"
-    }, {
-        date: "2025",
-        title: "175 Annivesery will happen, i guess",
-        cover: "https://unsplash.com/photos/KH8sFmJhKEI/download?ixid=M3wxMjA3fDB8MXx0b3BpY3x8Q0R3dXdYSkFiRXd8fHx8fDJ8fDE3MjQ2NTk5NzV8&force=true"
-    }];
+        date: "2025-03-15",
+        title: "Inter-country Cricket Match & Fun Games",
+        cover: "https://images.unsplash.com/photo-1574158622680-5e7b7b5d5a8f" // Cricket
+    },
+    {
+        date: "2025-03-15",
+        title: "2nd Day Triduum & Musical Show",
+        cover: "https://images.unsplash.com/photo-1503589111978-4c6e3c9b1b7b" // Musical performance
+    },
+    {
+        date: "2025-03-16",
+        title: "Inter-country Football Match & Fun Games",
+        cover: "https://images.unsplash.com/photo-1518770660439-4636190af475" // Football
+    },
+    {
+        date: "2025-03-16",
+        title: "3rd Day Triduum",
+        cover: "https://images.unsplash.com/photo-1573497491207-3f8f1e5b4d3f" // Religious gathering
+    },
+    {
+        date: "2025-03-17",
+        title: "Feast Mass, 175 Success Stories Book Release & Breakfast",
+        cover: "https://images.unsplash.com/photo-1589927986089-358123789b9d" // Breakfast gathering
+    },
+    {
+        date: "2025-03-17",
+        title: "Jubilee Dinner",
+        cover: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0" // Dinner setting
+    },
+    {
+        date: "2025-03-18",
+        title: "Kool Party at Poonary Beach",
+        cover: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0" // Beach party
+    },
+    {
+        date: "2025-03-19",
+        title: "Innovation Exhibition & Investor Invitation",
+        cover: "https://images.unsplash.com/photo-1581092162083-8a8d0a6a4f9a" // Exhibition
+    },
+    {
+        date: "2025-03-20",
+        title: "Exhibition & T-20 Match with Jaffna College (Big Match)",
+        cover: "https://images.unsplash.com/photo-1564694209613-29a9c5d5b72e" // T20 cricket match
+    },
+    {
+        date: "2025-03-21",
+        title: "Pilgrimage to Madhu",
+        cover: "https://images.unsplash.com/photo-1517511620798-4b6a8c9c6c9d" // Pilgrimage
+    },
+    {
+        date: "2025-03-22",
+        title: "Sports and Fun Event in Colombo (Day 1)",
+        cover: "https://images.unsplash.com/photo-1595152772832-0f4d8a6b4b5a" // Sports event
+    },
+    {
+        date: "2025-03-23",
+        title: "Sports and Fun Event in Colombo (Day 2)",
+        cover: "https://images.unsplash.com/photo-1595152772832-0f4d8a6b4b5a" // Sports event
+    },
+    {
+        date: "2025-07-21",
+        title: "Inter Batch Cricket Carnival (Day 1)",
+        cover: "https://images.unsplash.com/photo-1574158622680-5e7b7b5d5a8f" // Cricket
+    },
+    {
+        date: "2025-07-22",
+        title: "Inter Batch Cricket Carnival (Day 2)",
+        cover: "https://images.unsplash.com/photo-1574158622680-5e7b7b5d5a8f" // Cricket
+    },
+    {
+        date: "2025-07-23",
+        title: "Inter Batch Football & Basketball Carnival",
+        cover: "https://images.unsplash.com/photo-1518770660439- 4636190af475" // Football and basketball
+    },
+    {
+        date: "2025-07-24",
+        title: "College Prize Day",
+        cover: "https://images.unsplash.com/photo-1589927986089-358123789b9d" // Prize day celebration
+    },
+    {
+        date: "2025-07-25",
+        title: "Beach Party",
+        cover: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0" // Beach party
+    },
+    {
+        date: "2025-07-26",
+        title: "Carnival, Trade Fair & Innovation Exhibition",
+        cover: "https://images.unsplash.com/photo-1581092162083-8a8d0a6a4f9a" // Carnival
+    },
+    {
+        date: "2025-07-27",
+        title: "Cultural Events, Stamp Release, Musical Show & Jubilee Dinner",
+        cover: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0" // Cultural event
+    }
+];
 
 export default TIMELINE_EVENTS;
